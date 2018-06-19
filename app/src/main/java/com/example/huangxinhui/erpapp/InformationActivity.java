a01e9c7b46dbfe5d9d32647d40cc527f9112adcc
Acelyy <1084664238@qq.com>
2018-06-19 16:46:00 +0800

    代码格式化

--- app/src/main/java/com/example/huangxinhui/erpapp/InformationActivity.java
+++ app/src/main/java/com/example/huangxinhui/erpapp/InformationActivity.java
@@ -1,20 +1,13 @@
 package com.example.huangxinhui.erpapp;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -24,11 +17,12 @@ public class InformationActivity extends AppCompatActivity {
 
     @BindView(R.id.list)
     ExpandableListView list;
-    private String[] titles = {"基本信息","化学成分信息"};
+    private String[] titles = {"基本信息", "化学成分信息"};
     private String[][] ints = {
-            {"炉 号","生产日期","班 次","连铸机号","状 态","用 途","去 向","场 型","长 度"},
-            {"C","Sl","K","S","Mn","PE","H","O"}
+            {"炉 号", "生产日期", "班 次", "连铸机号", "状 态", "用 途", "去 向", "场 型", "长 度"},
+            {"C", "Sl", "K", "S", "Mn", "PE", "H", "O"}
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,9 +74,9 @@ public class InformationActivity extends AppCompatActivity {
 
             @Override
             public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-                View viewint = LayoutInflater.from(InformationActivity.this).inflate(R.layout.list_inf,viewGroup,false);
+                View viewint = LayoutInflater.from(InformationActivity.this).inflate(R.layout.list_inf, viewGroup, false);
                 TextView name = viewint.findViewById(R.id.name);
-                name.setText(getChild(i,i1).toString());
+                name.setText(getChild(i, i1).toString());
                 TextView edit = viewint.findViewById(R.id.edit);
                 return viewint;
             }
@@ -92,7 +86,7 @@ public class InformationActivity extends AppCompatActivity {
                 return true;
             }
         };
-        ExpandableListView expanlistview = (ExpandableListView)findViewById(R.id.list);
+        ExpandableListView expanlistview = (ExpandableListView) findViewById(R.id.list);
         expanlistview.setAdapter(adapter);
     }
 }
