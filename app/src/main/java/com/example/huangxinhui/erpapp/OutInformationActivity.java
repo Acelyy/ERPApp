e582ebd4677fb69f33d411507d7e1fad321561b6
huangxinhui <1084664238@qq.com>
2018-06-20 10:45:26 +0800

    提交

--- app/src/main/java/com/example/huangxinhui/erpapp/OutInformationActivity.java
+++ app/src/main/java/com/example/huangxinhui/erpapp/OutInformationActivity.java
@@ -3,11 +3,11 @@ package com.example.huangxinhui.erpapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SelcetActivity extends AppCompatActivity {
+public class OutInformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selcet);
+        setContentView(R.layout.activity_out_information);
     }
 }
--- app/src/main/java/com/example/huangxinhui/erpapp/ReceiveInformationActivity.java
+++ app/src/main/java/com/example/huangxinhui/erpapp/ReceiveInformationActivity.java
@@ -1,32 +1,33 @@
 package com.example.huangxinhui.erpapp;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class InformationActivity extends AppCompatActivity {
+public class ReceiveInformationActivity extends AppCompatActivity {
 
     @BindView(R.id.list)
     ExpandableListView list;
-    private String[] titles = {"基本信息", "化学成分信息"};
+    private String[] titles = {"基本信息"};
     private String[][] ints = {
-            {"炉 号", "生产日期", "班 次", "连铸机号", "状 态", "用 途", "去 向", "场 型", "长 度"},
-            {"C", "Sl", "K", "S", "Mn", "PE", "H", "O"}
+            {"炉 号", "生产日期", "班 次", "连铸机号", "状 态", "用 途", "去 向", "场 型", "长 度"}
     };
-
+    TextView title,name,num;
+    ImageView icon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_information);
+        setContentView(R.layout.activity_receive_information);
         ButterKnife.bind(this);
         ExpandableListAdapter adapter = new BaseExpandableListAdapter() {
             @Override
@@ -66,18 +67,20 @@ public class InformationActivity extends AppCompatActivity {
 
             @Override
             public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
-                View viewtitle = LayoutInflater.from(InformationActivity.this).inflate(R.layout.list_title, viewGroup, false);
-                TextView title = viewtitle.findViewById(R.id.title);
+                View viewtitle = LayoutInflater.from(ReceiveInformationActivity.this).inflate(R.layout.list_title, viewGroup, false);
+                title = viewtitle.findViewById(R.id.title);
+                icon = viewtitle.findViewById(R.id.icon);
+                icon.setImageResource(R.mipmap.eyes);
                 title.setText(getGroup(i).toString());
                 return viewtitle;
             }
 
             @Override
             public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-                View viewint = LayoutInflater.from(InformationActivity.this).inflate(R.layout.list_inf, viewGroup, false);
-                TextView name = viewint.findViewById(R.id.name);
+                View viewint = LayoutInflater.from(ReceiveInformationActivity.this).inflate(R.layout.list_information, viewGroup, false);
+                name = viewint.findViewById(R.id.name);
                 name.setText(getChild(i, i1).toString());
-                TextView edit = viewint.findViewById(R.id.edit);
+                num = viewint.findViewById(R.id.num);
                 return viewint;
             }
 
