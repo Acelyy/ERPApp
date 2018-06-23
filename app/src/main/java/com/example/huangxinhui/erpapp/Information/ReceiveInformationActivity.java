84c849e3f6608633008d8946bcaa388a22c0f2a5
Acelyy <1084664238@qq.com>
2018-06-23 09:25:13 +0800

    日程更新

--- app/src/main/java/com/example/huangxinhui/erpapp/Information/ReceiveInformationActivity.java
+++ app/src/main/java/com/example/huangxinhui/erpapp/Information/ReceiveInformationActivity.java
@@ -1,97 +1,36 @@
 package com.example.huangxinhui.erpapp.Information;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.ImageView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.huangxinhui.erpapp.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ReceiveInformationActivity extends AppCompatActivity {
 
-    @BindView(R.id.list)
-    ExpandableListView list;
-    private String[] titles = {"基本信息"};
-    private String[][] ints = {
-            {"炉 号", "生产日期", "班 次", "连铸机号", "状 态", "用 途", "去 向", "场 型", "长 度"}
-    };
-    TextView title,name,num;
-    ImageView icon;
+    @BindView(R.id.titleName)
+    TextView titleName;
+    @BindView(R.id.tl_tab)
+    TabLayout tlTab;
+    @BindView(R.id.pager)
+    ViewPager pager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_information);
         ButterKnife.bind(this);
-        ExpandableListAdapter adapter = new BaseExpandableListAdapter() {
-            @Override
-            public int getGroupCount() {
-                return titles.length;
-            }
 
-            @Override
-            public int getChildrenCount(int i) {
-                return ints[i].length;
-            }
+    }
 
-            @Override
-            public Object getGroup(int i) {
-                return titles[i];
-            }
-
-            @Override
-            public Object getChild(int i, int i1) {
-                return ints[i][i1];
-            }
-
-            @Override
-            public long getGroupId(int i) {
-                return i;
-            }
-
-            @Override
-            public long getChildId(int i, int i1) {
-                return i1;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return true;
-            }
-
-            @Override
-            public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
-                View viewtitle = LayoutInflater.from(ReceiveInformationActivity.this).inflate(R.layout.list_title, viewGroup, false);
-                title = viewtitle.findViewById(R.id.title);
-                icon = viewtitle.findViewById(R.id.icon);
-                icon.setImageResource(R.mipmap.eyes);
-                title.setText(getGroup(i).toString());
-                return viewtitle;
-            }
-
-            @Override
-            public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
-                View viewint = LayoutInflater.from(ReceiveInformationActivity.this).inflate(R.layout.list_information, viewGroup, false);
-                name = viewint.findViewById(R.id.name);
-                name.setText(getChild(i, i1).toString());
-                num = viewint.findViewById(R.id.num);
-                return viewint;
-            }
-
-            @Override
-            public boolean isChildSelectable(int i, int i1) {
-                return true;
-            }
-        };
-        ExpandableListView expanlistview = (ExpandableListView) findViewById(R.id.list);
-        expanlistview.setAdapter(adapter);
+    @OnClick(R.id.back)
+    public void onViewClicked() {
+        finish();
     }
 }
